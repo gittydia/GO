b93c52b0b3c0f7f86f311aeffcf369232cb222e0
Dianne Boholst <yeogiyeogi394@gmail.com>
2025-03-22 19:35:22 +0800

    feat: add user choice for signin and login options

--- bank/main.go
+++ bank/main.go
@@ -2,12 +2,39 @@ package main
 
 import (
 	"bank/function" // Changed the import path
+	"fmt"
 )
 
 func main() {
     var u1 function.SigninU
     var u2 function.LoginU
+    var choice int 
+
+    fmt.Println("Choose an option: ")
+    fmt.Println("1. Signin")
+    fmt.Println("2. Login")
+    fmt.Scanln(&choice)
+
+        
+    for {
+        if choice == 1 {
+            function.Signin(&u1)
+        } else if choice == 2 {
+            function.Login(&u2, &u1)
+        } else if choice == 3 {
+            fmt.Println("Exiting...")
+            break
+        } else {
+            fmt.Println("Invalid choice")
+        }
+
+        fmt.Println("\nChoose an option: ")
+        fmt.Println("1. Signin")
+        fmt.Println("2. Login")
+        fmt.Println("3. Exit")
+        fmt.Scanln(&choice)
+    }
+
     
-    function.Signin(&u1)
-    function.Login(&u2, &u1)
+   
 }
