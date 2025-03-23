8385e9915fc6636ebca6e4e82ebb8b6e6b56a0dc
Dianne Boholst <yeogiyeogi394@gmail.com>
2025-03-23 16:58:48 +0800

    feat: implement database connection and enhance user account management flow

--- bank/main.go
+++ bank/main.go
@@ -1,67 +1,121 @@
 package main
 
 import (
-	"bank/function" // Changed the import path
+	"bank/function"
+	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
-
 func main() {
-    var u1 function.SigninU
-    var u2 function.LoginU
-    var account function.UserAccount
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return
+	}
 
-    var choice int 
+	// Get database credentials from environment variables
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-    fmt.Println("Choose an option: ")
-    fmt.Println("1. Signin")
-    fmt.Println("2. Login")
-    fmt.Scanln(&choice)
-     
-    for {
-        if choice == 1 {
-            function.Signin(&u1)
-        } else if choice == 2 {
-            function.Login(&u2, &u1)
-            if u2.Email == u1.Email && u2.Password == u1.Password {
+	// Construct the database connection string
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 
-                fmt.Println("Welcome to the bank" + " " + u1.Name)
-                
-                
-                for {
-                    fmt.Println("\nChoose an option: ")
-                    fmt.Println("1. Deposit")
-                    fmt.Println("2. Withdraw")
-                    fmt.Println("3. Check balance")
-                    fmt.Println("4. Exit")
-                    fmt.Scanln(&choice)
-                    if choice == 1 {
-                        function.Deposit(&account)
-                    } else if choice == 2 {
-                        function.Withdraw(&account)
-                    } else if choice == 3 {
-                        function.CheckBalance(&account)
-                    } else if choice == 4 {
-                        fmt.Println("Exiting...")
-                        break
-                    } else {
-                        fmt.Println("Invalid choice")
-                    }
-                
-            }
-        } else if choice == 3 {
-            fmt.Println("Exiting...")
-            break
-        } else {
-            fmt.Println("Invalid choice")
-        }
+	// Establish database connection
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Println("Error connecting to the database:", err)
+		return
+	}
+	defer db.Close()
 
-        
-    }
-    fmt.Println("\nChoose an option: ")
-    fmt.Println("1. Signin")
-    fmt.Println("2. Login")
-    fmt.Println("3. Exit")
-    fmt.Scanln(&choice)
-}
+	var u1 function.SigninU
+	var u2 function.LoginU
+	var account function.UserAccount
+
+	var choice int
+
+	for {
+		fmt.Println("\nChoose an option: ")
+		fmt.Println("1. Signin")
+		fmt.Println("2. Login")
+		fmt.Println("3. Exit")
+		fmt.Print("Enter your choice: ")
+		_, err := fmt.Scanln(&choice)
+		if err != nil {
+			fmt.Println("Invalid input. Please try again.")
+			continue
+		}
+		if choice == 1 {
+			function.Signin(&u1, db)
+		} else if choice == 2 {
+			function.Login(&u2, db)
+			if u2.Email != "" && u2.Name != "" { // Ensure login was successful
+				fmt.Println("Welcome to the bank" + ", " + u2.Name + "!")
+
+				// Retrieve AccountID for the logged-in user
+				err := db.QueryRow("SELECT AccountID FROM Accounts WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE Email = ?)", u2.Email).Scan(&account.AccountID)
+				if err != nil {
+					if err == sql.ErrNoRows {
+						fmt.Println("Error: No account found for the user. Creating a new account...")
+						_, err = db.Exec("INSERT INTO Accounts (CustomerID, Balance) VALUES ((SELECT CustomerID FROM Customers WHERE Email = ?), 0)", u2.Email)
+						if err != nil {
+							fmt.Println("Error creating account for the user:", err)
+							continue
+						}
+						// Retrieve the newly created AccountID
+						err = db.QueryRow("SELECT AccountID FROM Accounts WHERE CustomerID = (SELECT CustomerID FROM Customers WHERE Email = ?)", u2.Email).Scan(&account.AccountID)
+						if err != nil {
+							fmt.Println("Error retrieving newly created account information:", err)
+							continue
+						}
+						fmt.Println("New account created successfully.")
+					} else {
+						fmt.Println("Error retrieving account information:", err)
+						continue
+					}
+				}
+
+				for {
+					fmt.Println("\nChoose an option: ")
+					fmt.Println("1. Deposit")
+					fmt.Println("2. Withdraw")
+					fmt.Println("3. Check balance")
+					fmt.Println("4. Exit")
+					fmt.Print("Enter your choice: ")
+					_, err := fmt.Scanln(&choice)
+					if err != nil {
+						fmt.Println("Invalid input. Please try again.")
+						continue
+					}
+					if choice == 1 {
+						function.Deposit(&account, db)
+					} else if choice == 2 {
+						function.Withdraw(&account, db)
+					} else if choice == 3 {
+						function.CheckBalance(&account, db)
+					} else if choice == 4 {
+						fmt.Println("Exiting...")
+						break
+					} else {
+						fmt.Println("Invalid choice")
+					}
+				}
+			} else {
+				fmt.Println("Login failed: User not recognized.")
+			}
+		} else if choice == 3 {
+			fmt.Println("Exiting...")
+			break
+		} else {
+			fmt.Println("Invalid choice")
+		}
+	}
 }
